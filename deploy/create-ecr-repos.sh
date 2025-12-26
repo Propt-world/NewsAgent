818daeeda5ed420592d83610556bfc51b91e6066
mahmooduhasan <mahmood.hasan@11prop.com>
2025-12-26 14:54:21 +0500

    Remove ECS deployment configurations from repository

--- deploy/create-ecr-repos.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Create ECR repositories for NewsAgent services
-aws ecr create-repository --repository-name newsagent-api --region us-east-1
-aws ecr create-repository --repository-name newsagent-scheduler --region us-east-1  
-aws ecr create-repository --repository-name newsagent-worker --region us-east-1
-
-echo "ECR repositories created successfully"
